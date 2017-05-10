c1a0477a203899ad8aa04296c9116289cccb2f44
Will Harding <harding.will@gmail.com>
2017-05-10 21:22:36 +0000

    BROKEN: logo and 2nd menu to the top

--- template-parts/navigation/navigation-top.php
+++ template-parts/navigation/navigation-top.php
@@ -10,12 +10,21 @@
 
 ?>
 <nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php _e( 'Top Menu', 'twentyseventeen' ); ?>">
+  <?php // TODO(wnh): EDITABLE: main nav icon ?>
+  <img id="header-icon" src="<?php echo get_parent_theme_file_uri( '/assets/images/rca_logo.png' ); ?>" >
 	<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false"><?php echo twentyseventeen_get_svg( array( 'icon' => 'bars' ) ); echo twentyseventeen_get_svg( array( 'icon' => 'close' ) ); _e( 'Menu', 'twentyseventeen' ); ?></button>
-	<?php wp_nav_menu( array(
-		'theme_location' => 'top',
-		'menu_id'        => 'top-menu',
-	) ); ?>
+  <div class="main-menu header-menu">
+    <?php wp_nav_menu( array(
+      'theme_location' => 'top',
+      'menu_id'        => 'top-menu',
+    ) ); ?>
+  </div>
 
+    <?php // TODO(wnh): EDITABLE: membership menu ?>
+  <ul class="membership-menu">
+    <li><a href="/membership">Membership</a></li>
+    <li><a href="/donate">Donate</a></li>
+  </ul>
 	<?php if ( ( twentyseventeen_is_frontpage() || ( is_home() && is_front_page() ) ) && has_custom_header() ) : ?>
 		<a href="#content" class="menu-scroll-down"><?php echo twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll down to content', 'twentyseventeen' ); ?></span></a>
 	<?php endif; ?>
